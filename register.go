88bcea149e04dfde7a7507709b4910b5dc9abfb3
Ivan <2103732+codebien@users.noreply.github.com>
2021-10-18 11:54:27 +0200

    main: Register the extension

---
+++ register.go
@@ -0,0 +1,12 @@
+package influxdb
+
+import (
+	"github.com/grafana/xk6-output-influxdb/pkg/influxdb"
+	"go.k6.io/k6/output"
+)
+
+func init() {
+	output.RegisterExtension("xk6-influxdb", func(p output.Params) (output.Output, error) {
+		return influxdb.New(p)
+	})
+}
