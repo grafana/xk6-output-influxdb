3cb8a3354522e3ab9c27472a5e6c804de611703d
jwcastillo <jwcastillo@gmail.com>
2022-08-14 09:28:50 -0400

    chore: add -T option to docker compose run command

--- docker-run.sh
+++ docker-run.sh
@@ -3,7 +3,7 @@
 #
 # This script simply executes a provided JavaScript test using
 # the local environment established with the `docker-compose`.
-# 
+#
 # Each execution is provided a unique tag to differentiate
 # discrete test runs within the Grafana dashboard.
 #
@@ -18,4 +18,4 @@ fi
 SCRIPT_NAME=$1
 TAG_NAME="$(basename -s .js $SCRIPT_NAME)-$(date +%s)"
 
-docker-compose run --rm k6 run -<$SCRIPT_NAME --tag testid=$TAG_NAME
+docker-compose run --rm -T k6 run -<$SCRIPT_NAME --tag testid=$TAG_NAME
